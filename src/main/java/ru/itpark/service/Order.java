bd8f4df4b515a548b62d9395502c14dac2fba5e1
Almir <almir.almiev@tatar.ru>
2019-10-01 17:35:40 +0300

    getter added

--- src/main/java/ru/itpark/service/Order.java
+++ src/main/java/ru/itpark/service/Order.java
@@ -1,11 +1,51 @@
 package ru.itpark.service;
 
 public class Order {
-    int boardingPrice = 60;
-    int kmPrice = 20;
-    int discountOnOrder = 1_000;
-    double discountPercent = .05;
-    int maxDiscount = 100;
+    private int boardingPrice = 60;
+    private int kmPrice = 20;
+    private int discountOnOrder = 1_000;
+    private double discountPercent = .05;
+    private int maxDiscount = 100;
+
+    public int getBoardingPrice() {
+        return boardingPrice;
+    }
+
+    public void setBoardingPrice(int boardingPrice) {
+        this.boardingPrice = boardingPrice;
+    }
+
+    public int getKmPrice() {
+        return kmPrice;
+    }
+
+    public void setKmPrice(int kmPrice) {
+        this.kmPrice = kmPrice;
+    }
+
+    public int getDiscountOnOrder() {
+        return discountOnOrder;
+    }
+
+    public void setDiscountOnOrder(int discountOnOrder) {
+        this.discountOnOrder = discountOnOrder;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public int getMaxDiscount() {
+        return maxDiscount;
+    }
+
+    public void setMaxDiscount(int maxDiscount) {
+        this.maxDiscount = maxDiscount;
+    }
 
     double calculateTripPrice(double lengthTrip){
         double result = lengthTrip * kmPrice + boardingPrice;
