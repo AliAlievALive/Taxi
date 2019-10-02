ecbc74b99a18b3e47425b36fdf473448bf31a4c5
Almir <almir.almiev@tatar.ru>
2019-10-02 10:59:47 +0300

    Start there do not watch before

--- src/main/java/ru/itpark/Taxi.java
+++
@@ -1,9 +0,0 @@
-package ru.itpark;
-
-public class Taxi {
-    public static void main(String[] args) {
-        Order toHome = new Order();
-        double lengthTrip = 100;
-        System.out.println(toHome.calculateTripPrice(lengthTrip));
-    }
-}
