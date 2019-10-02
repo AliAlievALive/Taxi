59568e338074f2476d36edb3ff25d72b3747cec6
Almir <almir.almiev@tatar.ru>
2019-10-02 11:22:49 +0300

    Error then run test with coverage

--- src/main/java/ru/itpark/service/Order.java
+++ src/main/java/ru/itpark/service/Order.java
@@ -1,11 +1,11 @@
 package ru.itpark.service;
 
 public class Order {
-    int boardingPrice = 60;
-    int kmPrice = 20;
-    int discountOnOrder = 1_000;
-    double discountPercent = .05;
-    int maxDiscount = 100;
+    private int boardingPrice = 60;
+    private int kmPrice = 20;
+    private int discountOnOrder = 1_000;
+    private double discountPercent = .05;
+    private int maxDiscount = 100;
 
     double calculateTripPrice(double lengthTrip){
         double result = lengthTrip * kmPrice + boardingPrice;
