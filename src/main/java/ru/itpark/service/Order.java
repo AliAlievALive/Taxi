ee4e371a6256e45d4b96e68429efff0e3d190975
Almir <almir.almiev@tatar.ru>
2019-10-19 08:31:20 +0300

    Don't know what with codecov

--- src/main/java/ru/itpark/service/Order.java
+++ src/main/java/ru/itpark/service/Order.java
@@ -4,7 +4,7 @@ public class Order {
     private int boardingPrice = 60;
     private int kmPrice = 20;
     private int discountOnOrder = 1_000;
-    private double discountPercent = 0.05;
+    private double discountPercent = .05;
     private int maxDiscount = 100;
 
     double calculateTripPrice(double lengthTrip){
